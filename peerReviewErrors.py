aef96b63376376dd6ec995f678779b59d4ab2843
philosophicalusername <93411235+philosophicalusername@users.noreply.github.com>
2021-10-29 22:17:14 -0500

    After Peer Review

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -1,34 +1,26 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <Chris Woods>
-# Creation Date: <October 29, 2021>
+# Author: Christopher Woods
+# Creation Date: October 29 2021
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
 import time
 
 def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
+        print('''You are in a land full of dragons. In front of you,
 	you see two caves. In one cave, the dragon is friendly
 	and will share his treasure with you. The other dragon
 	is greedy and hungry, and will eat you on sight.''')
-	print()
-
-def cave():
-	cave == ''
-
-def chosenCave():
-	chosenCave == ''
+print()
 
 def chooseCave():
-	cave = ''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-		cave = input()
-	return chosenCave
-
-
+        cave = ''
+        while cave != '1' and cave != '2':
+                print('Which cave will you go into? (1 or 2)')
+                cave = input()
+                return cave
 
 def checkCave(chosenCave):
 	print('You approach the cave...')
