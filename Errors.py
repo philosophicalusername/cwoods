cfbb9d5afb35b8cbe4c3bbffa3eab9b19453aa89
philosophicalusername <93411235+philosophicalusername@users.noreply.github.com>
2021-10-31 20:59:58 -0500

    Adding New
    Adding new Errors.py for review and changes

---
+++ Errors.py
@@ -0,0 +1,35 @@
+class Vehicle:
+
+    def _init__(self, make, model, vin, cylinders, color, lic_plate):
+        self.make = make
+        self.model = model
+        self.vin = vin
+        self.cylinders = cylinders
+        self.color = color
+    self.lic_plate = lic_plate
+
+    def createOwner(self, name):
+        persn = Person(name)
+
+    def setColor(color):
+        self.color = color
+
+    def setLicPlate(self, lic_plate):
+        self.lic_plate = lic_plate
+
+    def getMake(self):
+        return self.make
+
+    def describeDaVehicle(self):
+        print ('Make %s, Model %s, Color %s' % (self.make, self.model, self.color))
+
+
+
+ford1 = Vehicle('Ford', 'F150', '12345', 6, 'Black', 'ABC123')
+ford1.describeDaVehicle()
+ford1.setColor('Red')
+for1.describeDaVehicle()
+
+fordEx = Vehicle('Ford, 'Explorer', '454334', 4, 'Silver',87875)
+fordEx.describeDaVehicle()
+
