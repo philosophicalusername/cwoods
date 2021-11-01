51eeaeb90b1bf0913d4ec18d311b8aeb33dbec5d
philosophicalusername <93411235+philosophicalusername@users.noreply.github.com>
2021-10-31 21:39:18 -0500

    Update Errors.py
    Fixed missing definitions, corrected all data, added additional demonstrative changes at the end

--- Errors.py
+++ Errors.py
@@ -1,35 +1,56 @@
 class Vehicle:
 
-    def __init__(self, make, model, vin, cylinders, color, lic_plate):
+    def __init__(self, owner, make, model, vin, cylinders, color, licplate):
+        self.owner = owner
         self.make = make
         self.model = model
         self.vin = vin
         self.cylinders = cylinders
         self.color = color
-        self.lic_plate = lic_plate
+        self.licplate = licplate
 
     def createOwner(self, name):
-        persn = Person(name)
+        self.owner = owner(name)
+
+    def getMake(self, make):
+        self.make = make
+
+    def getModel(self, model):
+        self.model = model
+
+    def getVin(self, vin):
+        self.vin = vin
+        
+    def getCylinders(self, cylinders):
+        self.cylinders = cylinders
 
     def setColor(self, color):
         self.color = color
 
-    def setLicPlate(self, lic_plate):
-        self.lic_plate = lic_plate
-
-    def getMake(self, make):
-        return self.make
+    def setLicPlate(self, licplate):
+        self.licplate = licplate
 
     def describeTheVehicle(self):
-        print ('Make %s, Model %s, Color %s' % (self.make, self.model, self.color))
+        print ('Owner %s, Make %s, Model %s, Vin %s, Cylinders %s, Color %s, LicPlate %s' % (self.owner, self.make, self.model, self.vin, self.cylinders, self.color, self.licplate))
 
 
 
-ford1 = Vehicle('Ford', 'F150', '12345', 6, 'Black', 'ABC123')
+ford1 = Vehicle('Professor Kumar', 'Ford', 'F150', '1FTEW1E53LFA80314', 6, 'Black', 'ABC123')
 ford1.describeTheVehicle()
+
+ford2 = Vehicle('Mond Treje', 'Ford', 'Mustang', '1FA6P8CF5K5111613', 8, 'Red', 'G0F4ST')
+ford1.describeTheVehicle()
+
+ford3 = Vehicle('Jea Retre', 'Ford', 'Explorer', '1FATP8FF6G5312006', 8, 'Silver', 'SCRM0M')
+ford3.describeTheVehicle()
+print("")
+print("After Paintjobs:")
+print("")
 ford1.setColor('Red')
 ford1.describeTheVehicle()
 
-fordEx = Vehicle('Ford', 'Explorer', '454334', 4, 'Silver',87875)
-fordEx.describeTheVehicle()
+ford2.setColor('Neon Green')
+ford2.describeTheVehicle()
 
+ford3.setColor('Purple')
+ford3.describeTheVehicle()
